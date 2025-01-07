6043246f1333413bc9925c1d9c2cce4b08b0ee60
Gulzoda0102 <zodait2024@gmail.com>
2025-01-07 19:09:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+// const username
+
+// camel case => userName
+// snake case => user_name
+// pascal case => user-name
+
+
+// const userName = "Gulzoda";
+// console.log(userName.toUpperCase()); kotta harf
+// console.log(userName.toLocaleLowerCase()); kichik harf
+// console.log(userName.length); harflar uzunligi soni
+// console.log(userName.trimStart()); ostup chopadi boshidan
+// console.log(userName.trimEnd()); ostup oxiridan 
+// console.log(userName.trimStart()); hamma joydan ostup
+// console.log(userName.indexOf());
+// console.log(userName[0]); index noldan boshlanadi va number bersak oshanondex turgan harf
+// console.log(userName.slice(4);  sozlarni kesadi
+// console.log(userName.includes);("G" , 8); string ichida harf bor yoki yoqligini tekshiradi 
+// console.log(userName.charCodeAt(3)); raqam beriladi va osha harfni ascii table dagi raqam 
+// console.log(userName.charAt(3));
+// console.log(userName[2]);
+
+// const userName = prompt("Enter user name");
+// const result = confirm("if you click ok your name will be bigger \n if you click cansel your name will be smaller");
+// console.log(userName);
+// if(userName == ""){
+
+// }
+
+// if(result == true){
+//     alert(userName.toUpperCase());
+// }else{
+//     alert(userName.toLowerCase());
+// }
+// alert(userName.toUpperCase());
+// alert(userName.toLowerCase());
+
+
+
+
+
+
+
+const name = prompt("Ismingizni kiriting:");
+const spaceId = prompt("Space ID ni kiriting:");
+const spacePassword = prompt("Space Password ni kiriting:");
+
+const userObject = {
+  name: name,
+  id: spaceId,
+  password: spacePassword
+};
+
+console.log(userObject);
+
+
+
+
+
+
+
+
+
